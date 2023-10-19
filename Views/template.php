5ca358165a887a12324721b3aa31e8866796319c
Clément Bartholomé <clementoss@gmail.com>
2023-10-19 18:37:49 +0200

    style

--- Views/template.php
+++ Views/template.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body class="container">
+<body class="container d-flex flex-column vh-100">
     <header class="py-4">
         <h1><i class="fas fa-basketball-ball"></i> Ballers Shop</h1>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -40,7 +40,7 @@
     <main>
         <?= $content ?>
     </main>
-    <footer class="bg-primary text-white py-4 mt-4">
+    <footer class="bg-primary text-white py-4 mt-auto">
         <div class="container">
             <div class="row">
                 <div class="col-lg-3 col-md-6 mb-4">
@@ -82,5 +82,6 @@
     </footer>
     <script src="https://kit.fontawesome.com/7c85ecb699.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
 </body>
 </html>
