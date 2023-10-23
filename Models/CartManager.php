e23c841420d457c7f0e6adb0a40847a93fd34b61
Clément Bartholomé <clementoss@gmail.com>
2023-10-23 18:22:22 +0200

    add getUserCart, getCartContents, getCartItem, updateCartItemQuantity and removeProductFromCart

--- Models/CartManager.php
+++ Models/CartManager.php
@@ -1,10 +1,45 @@
 <?php 
 
 class CartManager extends Model {
+
+    public function getUserCart($user_id) {
+        $query = "SELECT * FROM cart WHERE user_id = :user_id";
+        $cartContents = $this->executeRequest($query, [':user_id' => $user_id]);
+        return $cartContents->fetchAll();
+    }
+
     public function addProductToCart($productData) {
         $query = "INSERT INTO cart (product_id, user_id, quantity) 
                   VALUES (:product_id, :user_id, :quantity)";
         $this->executeRequest($query, $productData);
 
+    }
+
+    public function getCartContents($user_id) {
+        $query = "SELECT * FROM cart WHERE user_id = :user_id";
+        $cartContents = $this->executeRequest($query, [':user_id' => $user_id]);
+        return $cartContents->fetchAll();
+    }
+
+    public function getCartItem($user_id, $product_id) {
+        $query = "SELECT * FROM cart WHERE user_id = :user_id AND product_id = :product_id";
+        $cartItem = $this->executeRequest($query, [
+            ':user_id' => $user_id,
+            ':product_id' => $product_id
+        ]);
+        return $cartItem->fetch();
+    }
+
+    public function updateCartItemQuantity($cart_id, $quantity) {
+        $query = "UPDATE cart SET quantity = :quantity WHERE id = :id";
+        $this->executeRequest($query, [
+            ':quantity' => $quantity,
+            ':id' => $cart_id
+        ]);
+    }
+    
+    public function removeProductFromCart($cart_id) {
+        $query = "DELETE FROM cart WHERE id = :id";
+        $this->executeRequest($query, [':id' => $cart_id]);
     }
 }
