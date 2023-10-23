d9943a7e4ac68ba70a042b94f9d8d6037d2df546
Clément Bartholomé <clementoss@gmail.com>
2023-10-23 16:33:47 +0200

    add getCategoryIdByName, addCategory and associateProductWithCategory

--- Models/CategoryManager.php
+++ Models/CategoryManager.php
@@ -6,4 +6,37 @@ class CategoryManager extends Model {
         $result = $this->executeRequest($query);
         return $result->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function getCategoryIdByName($categoryName) {
+        $query = "SELECT id FROM categories WHERE name = :categoryName";
+        $params = [':categoryName' => $categoryName];
+        $result = $this->executeRequest($query, $params);
+        
+        $category = $result->fetch(PDO::FETCH_ASSOC);
+        
+        if ($category) {
+            return $category['id'];
+        } else {
+            return null;
+        }
+    }
+
+    public function addCategory($categoryName) {
+        $query = "INSERT INTO categories (name) VALUES (:categoryName)";
+        $params = [':categoryName' => $categoryName];
+        $this->executeRequest($query, $params);
+
+
+        return $this->getCategoryIdByName($categoryName);
+    }
+
+    public function associateProductWithCategory($productId, $categoryId) {
+        $query = "INSERT INTO categories_products (id, category_id, product_id) VALUES (:id, :categoryId, :productId)";
+        $params = [
+            ':id' => 0,
+            ':categoryId' => $categoryId,
+            ':productId' => $productId
+        ];
+        $this->executeRequest($query, $params);
+    }
 }
