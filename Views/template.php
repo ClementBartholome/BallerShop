4ce9605e7d69cac25875086eef2e6eef15ccc994
Clément Bartholomé <clementoss@gmail.com>
2023-10-23 18:22:58 +0200

    conditional rendering for cart

--- Views/template.php
+++ Views/template.php
@@ -20,13 +20,13 @@
                 </button>
                 <div class="collapse navbar-collapse col-sm-12 col-md-6" id="navbarNav">
                     <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                    <a class="nav-link" href="/Ballers/cart">
-                        <img src="images/basket.svg" alt="Panier" width="16" >
-                        Panier
-                    </a>
-                    </li>
                         <?php if (isset($_SESSION['userIsLoggedIn']) && $_SESSION['userIsLoggedIn']) { ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/Ballers/cart">
+                                    <img src="images/basket.svg" alt="Panier" width="16" >
+                                    Panier
+                                </a>
+                            </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="/Ballers/logout"><i class="fas fa-user"></i> Se Déconnecter</a>
                             </li>
@@ -91,5 +91,6 @@
     <script src="https://kit.fontawesome.com/7c85ecb699.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="js/script.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 </body>
 </html>
